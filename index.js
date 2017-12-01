6d99cd082b07689ab398a2d2fef2a09ca5de4556
daria marczak <marczak.ria@gmail.com>
2017-12-01 18:05:21 +0100

    modified

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ fs.readFile("./text.txt", "utf-8", function (err, data) {
     });
 });
 
-fs.readdir("../", "utf-8", function (err, files) {
+fs.readdir("./", "utf-8", function (err, files) {
     fs.writeFile("./message.txt", files, "utf-8", function(err){
         if (err) throw err;
         console.log("Saved");
